826d1308ad4c04773520eaa9354e99fde3360549
AKirosan <kirilenkoandr@yandex.ru>
2022-02-08 13:36:28 +0400

    edit workflow and docker-compose

--- calulate/calculate/settings.py
+++ calulate/calculate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-e1fx+7)uo!qbp*^9lr0n=380r1o8n=9)v3%t1=^zx3f)-iz4&m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.estimate.ml', 'estimate.ml', '80.249.151.80', ' localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.estimate.ml', 'estimate.ml', '80.249.151.80', ' localhost', '127.0.0.1', 'web']
 
 
 # Application definition
